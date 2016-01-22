249dc03c310eca78b15a09eb5cc4b8ba88cd945d
William Lim <manvinedi@gmail.com>
2016-01-22 17:58:45 +0800

    update skeleton

--- js/script.js
+++ js/script.js
@@ -1,48 +1,22 @@
-//http://cssmenumaker.com/blog/responsive-menu-tutorial
-//http://stackoverflow.com/questions/24259016/responsive-menu-show-and-hide-on-click
+/*if (!juis) {
+  var juis = {};
+}
 
-var samples = {};
-
-samples.clickmenu = {
+juis.onebuttonmegamenu = {
 
   init: function() {
-      document.querySelector(".menubutton").addEventListener("click", function() {
-      document.querySelector(".menubutton ul").classList.toggle(".menu");
-      return false;
+      document.querySelector(".onebuttonmegamenucontainer").addEventListener("click", function() {
+      document.querySelector(".onebuttonmegamenucontainer ul").classList.add(".menu");
       })
+  }
+
+  bind: function() {
+
   }
 
 };
 
+
 document.addEventListener("DOMContentLoaded", function() {
-  samples.clickmenu.init();
-});
-
-
-/*$(function() {
-  var menuVisible = false;
-  $('#menuBtn').click(function() {
-    if (menuVisible) {
-      $('#myMenu').css({'display':'none'});
-      menuVisible = false;
-      return;
-    }
-    $('#myMenu').css({'display':'block'});
-    menuVisible = true;
-  });
-  $('#myMenu').click(function() {
-    $(this).css({'display':'none'});
-    menuVisible = false;
-  });
-});
-*/
-
-/*
-div.menu-bar ul ul {
-  display: none;
-}
-
-div.menu-bar li:hover > ul {
-  display: block;
-}
-*/
+  juis.onebuttonmegamenu.init();
+});*/
